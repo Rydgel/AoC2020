109650be9635d9986e86d14ed80907429f373448
Jérôme Mahuet <jerome.mahuet@gmail.com>
2020-12-02 17:41:51 +0100

    day 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,18 +1,40 @@
+use itertools::Itertools;
+
 const INPUT: &str = include_str!("../input.txt");
 
-fn parse_input(input: &str) -> Vec<i32> {
+fn parse_input(input: &str) -> Vec<&str> {
     input
-        .split_whitespace()
-        .map(|s| s.parse().unwrap())
-        .collect()
+        .split_terminator('\n')
+        .collect_vec()
 }
 
-fn part1(xs: &Vec<i32>) -> i32 {
-    0
+fn is_valid_1(line: &str) -> bool {
+    let (min, max, ch, pass): (usize, usize, char, String) =
+            serde_scan::scan!("{}-{} {}: {}" <- line).unwrap();
+    let count = pass.matches(ch).count();
+    min <= count && count <= max
 }
 
-fn part2(xs: &Vec<i32>) -> i32 {
-    0
+fn part1(lines: &[&str]) -> usize {
+    lines
+        .iter()
+        .filter(|line| is_valid_1(line))
+        .count()
+}
+
+fn is_valid_2(line: &str) -> bool {
+    let (x, y, ch, pass): (usize, usize, char, String) =
+            serde_scan::scan!("{}-{} {}: {}" <- line).unwrap();
+    let ch_x = pass.chars().nth(x - 1).unwrap();
+    let ch_y = pass.chars().nth(y - 1).unwrap();
+    (ch_x == ch) ^ (ch_y == ch)
+}
+
+fn part2(lines: &[&str]) -> usize {
+    lines
+        .iter()
+        .filter(|line| is_valid_2(line))
+        .count()
 }
 
 fn main() {
@@ -29,12 +51,14 @@ mod tests {
     #[test]
     fn p1() {
         let input: Vec<_> = parse_input(TEST_INPUT);
-        
+        let result = part1(&input);
+        assert_eq!(result, 2);
     }
 
     #[test]
     fn p2() {
         let input: Vec<_> = parse_input(TEST_INPUT);
-        
+        let result = part2(&input);
+        assert_eq!(result, 1);
     }
 }
