06ad8abf703191200345d78bbb504ecdc9d03a17
Jérôme Mahuet <jerome.mahuet@gmail.com>
2020-12-05 16:34:12 +0100

    refactoring

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,22 +1,20 @@
 use itertools::Itertools;
-use std::collections::HashSet;
 
 const INPUT: &str = include_str!("../input.txt");
 
-fn parse_input(input: &str) -> Vec<(&str, &str)> {
-    input.lines().map(|s| s.split_at(7)).collect_vec()
+fn parse_input(input: &str) -> Vec<&str> {
+    input.lines().collect_vec()
 }
 
-fn convert_decimal(seats: &(&str, &str)) -> isize {
-    let (rows, columns) = seats;
-    let binary_rows = rows.replace("F", "0").replace("B", "1");
-    let binary_columns = columns.replace("L", "0").replace("R", "1");
-    let int_rows = isize::from_str_radix(&binary_rows, 2).expect("Not a valid binary number");
-    let int_columns = isize::from_str_radix(&binary_columns, 2).expect("Not a valid binary number");
-    int_rows * 8 + int_columns
+fn convert_decimal(seats: &str) -> isize {
+    seats.chars().fold(0, |acc, x| match x {
+        'F' | 'L' => acc << 1,
+        'B' | 'R' => acc << 1 | 1,
+        _ => unreachable!(),
+    })
 }
 
-fn part1(lines: &[(&str, &str)]) -> isize {
+fn part1(lines: &[&str]) -> isize {
     lines
         .iter()
         .map(|seats| convert_decimal(seats))
@@ -24,12 +22,14 @@ fn part1(lines: &[(&str, &str)]) -> isize {
         .expect("No seats found in input")
 }
 
-fn part2(lines: &[(&str, &str)]) -> isize {
-    let seats: HashSet<isize> = lines.iter().map(|seats| convert_decimal(seats)).collect();
-    let min_seat = seats.iter().min().expect("No seats found");
-    let max_seat = seats.iter().max().expect("No seats found");
-    let full_seats: HashSet<isize> = (*min_seat..*max_seat).collect();
-    *full_seats.difference(&seats).next().expect("no result")
+fn part2(lines: &[&str]) -> isize {
+    let seats = lines
+        .iter()
+        .map(|seats| convert_decimal(seats))
+        .collect_vec();
+    let min = seats.iter().min().expect("No seats found");
+    let max = seats.iter().max().expect("No seats found");
+    (*min..=*max).sum::<isize>() - seats.iter().sum::<isize>()
 }
 
 fn main() {
